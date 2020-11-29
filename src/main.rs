1d5104ffb696cec9109a9bd179e4e09fd4a0dfad
Enrico Lefass <enrico.lefass@mailbox.org>
2020-11-29 10:53:26 +0100

    Adding more menu entries to the app (still no real functionality)
     *  Adding a lot of menu entries plus enums for the shortcuts and adding
        a app.wait() match tree.
     *  Ctrl+q finally quits the app without warning (TODO should be message an
        autosave).
    
    Signed-off-by: Enrico Lefass <enrico.lefass@mailbox.org>

--- src/main.rs
+++ src/main.rs
@@ -8,33 +8,49 @@
 
 use fltk::{app::*, dialog::*, menu::*, window::*};
 
+// Adding something here, needs to be added in the match tree of
+// app.wait() too!
 #[derive(Copy, Clone)]
 pub enum Message {
     About,
+    AddUTF8Signature,
     Backup,
     Changed,
     CheckForUpdate,
     Copy,
+    CopyURLOfLocalThought,
+    CopyURLOfWebThought,
+    CloseTab,
+    CloseWindow,
     Cut,
+    DataIntegrityScan,
+    DoubleThought,
     Export,
+    HideAllParents,
     Import,
+    ManageEndPoints,
     NewCerebro,
     NewTab,
     NewWindow,
+    None,
     Open,
     Paste,
     Quit,
     RenameCerebro,
     Save,
     SaveAs,
+    SearchReplace,
+    SelectAll,
+    ShowAllParents,
     Statistics,
-    Utilities,
+    Undo,
+    Redo,
 }
 
 fn main() {
     const INITIAL_WIDTH: i32 = 864;
     const INITIAL_HEIGHT: i32 = 360;
-    const INITIAL_MENU_HEIGHT: i32 = 21;
+    const INITIAL_MENU_HEIGHT: i32 = 23;
 
     // The app variable is immutable. This is the entry point of the
     // application.
@@ -42,12 +58,12 @@ fn main() {
     // based config file.
     // Loads all fonts known to the system for convenience later on.
     let app = App::default()
-        .with_scheme(AppScheme::Base)
+        .with_scheme(AppScheme::Gleam)
         .load_system_fonts();
 
     // Collect the cli arguments.
-    // TODO:    Document each of them!
-    let args: Vec<String> = std::env::args().collect();
+    // TODO:    Implement and document each of them!
+    //let args: Vec<String> = std::env::args().collect();
 
     let (s, r) = channel::<Message>();
 
@@ -59,7 +75,23 @@ fn main() {
     let mut menu = SysMenuBar::new(0, 0, INITIAL_WIDTH, INITIAL_MENU_HEIGHT, "cerebro menu");
 
     menu.add_emit(
-        "&File/New...\t",
+        "&File/New Tab\t",
+        Shortcut::Ctrl | 't',
+        MenuFlag::Normal,
+        s,
+        Message::NewTab,
+    );
+
+    menu.add_emit(
+        "&File/New Window\t",
+        Shortcut::Ctrl | Shortcut::Shift | 'n',
+        MenuFlag::Normal,
+        s,
+        Message::NewTab,
+    );
+
+    menu.add_emit(
+        "&File/New Cerebro\t",
         Shortcut::Ctrl | 'n',
         MenuFlag::Normal,
         s,
@@ -67,27 +99,107 @@ fn main() {
     );
 
     menu.add_emit(
-        "&File/Open...\t",
-        Shortcut::Ctrl | 'o',
+        "&File/Backup to cerebro archive\t",
+        Shortcut::None,
         MenuFlag::Normal,
         s,
-        Message::Open,
+        Message::Backup,
     );
 
     menu.add_emit(
-        "&File/Save\t",
-        Shortcut::Ctrl | 's',
+        "&File/Import\t",
+        Shortcut::None,
         MenuFlag::Normal,
         s,
-        Message::Save,
+        Message::Import,
     );
 
     menu.add_emit(
-        "&File/Save as...\t",
+        "&File/Export\t",
+        Shortcut::None,
+        MenuFlag::Normal,
+        s,
+        Message::Export,
+    );
+
+    menu.add_emit(
+        "&File/Rename cerebro\t",
+        Shortcut::None,
+        MenuFlag::Normal,
+        s,
+        Message::RenameCerebro,
+    );
+
+    menu.add_emit(
+        "&File/Statistics\t",
+        Shortcut::None,
+        MenuFlag::Normal,
+        s,
+        Message::Statistics,
+    );
+
+    menu.add_emit(
+        "&File/Utilities/Manage endpoints\t",
+        Shortcut::None,
+        MenuFlag::Normal,
+        s,
+        Message::ManageEndPoints,
+    );
+
+    menu.add_emit(
+        "&File/Utilities/Search and Replace\t",
+        Shortcut::None,
+        MenuFlag::Normal,
+        s,
+        Message::SearchReplace,
+    );
+
+    menu.add_emit(
+        "&File/Utilities/Data integrity scan\t",
+        Shortcut::None,
+        MenuFlag::Normal,
+        s,
+        Message::DataIntegrityScan,
+    );
+
+    menu.add_emit(
+        "&File/Utilities/Add UTF-8 signature to notes\t",
+        Shortcut::None,
+        MenuFlag::Normal,
+        s,
+        Message::AddUTF8Signature,
+    );
+
+    menu.add_emit(
+        "&File/Utilities/Show all parents\t",
+        Shortcut::None,
+        MenuFlag::Normal,
+        s,
+        Message::ShowAllParents,
+    );
+
+    menu.add_emit(
+        "&File/Utilities/Hide all parents\t",
+        Shortcut::None,
+        MenuFlag::Normal,
+        s,
+        Message::HideAllParents,
+    );
+
+    menu.add_emit(
+        "&File/Close Tab\t",
         Shortcut::Ctrl | 'w',
+        MenuFlag::Normal,
+        s,
+        Message::CloseTab,
+    );
+
+    menu.add_emit(
+        "&File/Close Window\t",
+        Shortcut::Alt | 'w',
         MenuFlag::MenuDivider,
         s,
-        Message::SaveAs,
+        Message::CloseWindow,
     );
 
     menu.add_emit(
@@ -96,6 +208,22 @@ fn main() {
         MenuFlag::Normal,
         s,
         Message::Quit,
+    );
+
+    menu.add_emit(
+        "&Edit/Undo\t",
+        Shortcut::Ctrl | 'z',
+        MenuFlag::Normal,
+        s,
+        Message::Undo,
+    );
+
+    menu.add_emit(
+        "&Edit/Redo\t",
+        Shortcut::Ctrl | 'y',
+        MenuFlag::MenuDivider,
+        s,
+        Message::Redo,
     );
 
     menu.add_emit(
@@ -115,11 +243,43 @@ fn main() {
     );
 
     menu.add_emit(
+        "&Edit/Copy URL of local thought\t",
+        Shortcut::None,
+        MenuFlag::Normal,
+        s,
+        Message::CopyURLOfLocalThought,
+    );
+
+    menu.add_emit(
+        "&Edit/Copy URL of web thought\t",
+        Shortcut::None,
+        MenuFlag::Normal,
+        s,
+        Message::CopyURLOfWebThought,
+    );
+
+    menu.add_emit(
+        "&Edit/Double thought\t",
+        Shortcut::None,
+        MenuFlag::MenuDivider,
+        s,
+        Message::DoubleThought,
+    );
+
+    menu.add_emit(
         "&Edit/Paste\t",
         Shortcut::Ctrl | 'v',
         MenuFlag::Normal,
         s,
         Message::Paste,
+    );
+
+    menu.add_emit(
+        "&Edit/Select all\t",
+        Shortcut::Ctrl | 'a',
+        MenuFlag::MenuDivider,
+        s,
+        Message::SelectAll,
     );
 
     menu.add_emit(
@@ -141,5 +301,43 @@ fn main() {
     w.make_resizable(true);
     w.end();
     w.show();
-    app.run().unwrap();
+    while app.wait() {
+        match r.recv() {
+            // TODO: Fill all of them with life!
+            Some(Message::About) => println!("About"),
+            Some(Message::AddUTF8Signature) => println!("AddUTF8Signature"),
+            Some(Message::Backup) => println!("Backup"),
+            Some(Message::Changed) => println!("Changed"),
+            Some(Message::CheckForUpdate) => println!("CheckForUpdate"),
+            Some(Message::Copy) => println!("Copy"),
+            Some(Message::CopyURLOfLocalThought) => println!("CopyURLOfLocalThought"),
+            Some(Message::CopyURLOfWebThought) => println!("CopyURLOfWebThought"),
+            Some(Message::CloseTab) => println!("CloseTab"),
+            Some(Message::CloseWindow) => println!("CloseWindow"),
+            Some(Message::Cut) => println!("Cut"),
+            Some(Message::DataIntegrityScan) => println!("DataIntegrityScan"),
+            Some(Message::DoubleThought) => println!("DoubleThought"),
+            Some(Message::Export) => println!("Export"),
+            Some(Message::HideAllParents) => println!("HideAllParents"),
+            Some(Message::Import) => println!("Import"),
+            Some(Message::ManageEndPoints) => println!("ManageEndPoints"),
+            Some(Message::NewCerebro) => println!("NewCerebro"),
+            Some(Message::NewTab) => println!("NewTab"),
+            Some(Message::NewWindow) => println!("NewWindow"),
+            Some(Message::None) => println!("None"),
+            Some(Message::Open) => println!("Open"),
+            Some(Message::Paste) => println!("Paste"),
+            Some(Message::Quit) => app.quit(),
+            Some(Message::RenameCerebro) => println!("RenameCerebro"),
+            Some(Message::Save) => println!("Save"),
+            Some(Message::SaveAs) => println!("SaveAs"),
+            Some(Message::SearchReplace) => println!("SearchReplace"),
+            Some(Message::SelectAll) => println!("SelectAll"),
+            Some(Message::ShowAllParents) => println!("ShowAllParents"),
+            Some(Message::Statistics) => println!("Statistics"),
+            Some(Message::Undo) => println!("Undo"),
+            Some(Message::Redo) => println!("Redo"),
+            None => (),
+        }
+    }
 }
